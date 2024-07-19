7da1a325f0eb041e6c330b940c1ef24cb7e0556b
Juneict <junemoenyinyiict@gmail.com>
2024-07-19 14:44:17 +0700

    add : auth feature with OTP

---
+++ app/Notifications/OtpNotification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class OtpNotification extends Notification
+{
+    use Queueable;
+
+    protected $otp;
+
+    public function __construct($otp)
+    {
+        $this->otp = $otp;
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Your OTP for password reset is: ' . $this->otp->token)
+                    ->line('This OTP will expire in ' . $this->otp->expiry . ' minutes.')
+                    ->line('If you did not request a password reset, no further action is required.');
+    }
+}
